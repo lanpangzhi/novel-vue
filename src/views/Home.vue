2cade0a132e1f9d8865348864e9a5d019eafee22
lanpangzhi <875727617@qq.com>
2018-06-01 18:10:39 +0800

    首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="欢迎访问蓝胖纸小说网站"/>
+    <!-- 头部 -->
+    <Header title="首页" :left="false"/>
+    <!-- 头部 end -->
+    <!-- 下拉刷新 -->
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <List :data="indexData" top="top" bottom="bottom"/>
+    </van-pull-refresh>
+    <!-- 下拉刷新 end -->
+    <!--  导航 -->
+    <Nav/>
+    <!--  导航 end -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import List from "@/components/List.vue";
+import Nav from "@/components/base/Nav.vue";
+import Loading from "@/components/base/Loading.vue";
+import Header from "@/components/base/Header.vue";
+import { Toast } from "vant";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    List,
+    Nav,
+    Loading,
+    Header
+  },
+  data() {
+    return {
+      indexData: [],
+      isLoading: false
+    };
+  },
+  created() {
+    this.pageInit();
+  },
+  methods: {
+    pageInit() {
+      this.getIndexData();
+    },
+    onRefresh() {
+      this.getIndexData();
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 500);
+    },
+    async getIndexData() {
+      let data = await this.axios.get("/index");
+      if (data.flag === 1) {
+        this.indexData = data.books;
+      } else {
+        Toast.fail(data.msg);
+      }
+    }
   }
 };
 </script>
