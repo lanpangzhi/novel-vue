9fb872fcf02290f8dc147b516fc03251d15cc45f
lanpangzhi <875727617@qq.com>
2018-06-01 18:09:12 +0800

    增加公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -15,6 +11,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 }
 #nav {
   padding: 30px;
