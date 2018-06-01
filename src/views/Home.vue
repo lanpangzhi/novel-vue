65437c18fb44597be8aa40eecef0f1e9caae0971
lanpangzhi <875727617@qq.com>
2018-06-01 11:44:41 +0800

    @vue/cli初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <HelloWorld msg="欢迎访问蓝胖纸小说网站"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
