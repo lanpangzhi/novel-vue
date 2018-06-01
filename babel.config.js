0e1d7bd296ef6aed5faf885f3a281e5da42f3b81
lanpangzhi <875727617@qq.com>
2018-06-01 18:08:04 +0800

    增加vant预设

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ["@vue/app"]
+  presets: ["@vue/app"],
+  "plugins": [
+    ["import", {
+      "libraryName": "vant",
+      "libraryDirectory": "es",
+      "style": true
+    }]
+  ]
 };
