db89e736dc720a28bd9a0f567aa2ae20240a027a
lanpangzhi <875727617@qq.com>
2018-06-01 18:09:53 +0800

    加载需要的插件

--- src/main.js
+++ src/main.js
@@ -3,10 +3,26 @@ import App from "./App.vue";
 import router from "./router/router";
 import store from "./store/store";
 import axios from "axios";
+/* eslint-disable */
+import { Loading, Tabbar, TabbarItem, NavBar, Icon, PullRefresh, Lazyload } from "vant";
+
+Vue.use(Loading);
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(NavBar);
+Vue.use(Icon);
+Vue.use(Lazyload);
+Vue.use(PullRefresh);
 
 Vue.config.productionTip = false;
 // 全局vue添加axios
 Vue.prototype.axios = axios;
+// 设置默认请求路径
+axios.defaults.baseURL = "https://api.langpz.com";
+//拦截器要求axios返回的数据是res.data
+axios.interceptors.response.use(res => {
+  return res.data;
+});
 
 new Vue({
   router,
