ec492b8166fd760d6bf8aa0936498438f86af53e
lanpangzhi <875727617@qq.com>
2018-06-01 18:16:17 +0800

    头部

---
+++ src/components/base/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <van-nav-bar :title="title"  :left-arrow="left" fixed @click-left="onClickLeft" @click-right="onClickRight">
+        <van-icon name="search" slot="right" />
+    </van-nav-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["left", "title"],
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1);
+    },
+    onClickRight() {
+      this.$router.push("/serach");
+    }
+  }
+};
+</script>
