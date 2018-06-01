539e2eaa66c2211eb6500780e2dbb1269b7736bc
lanpangzhi <875727617@qq.com>
2018-06-01 18:16:30 +0800

    导航

---
+++ src/components/base/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="search" to="/serach">搜索</van-tabbar-item>
+      <van-tabbar-item icon="records" to="/serach">排行</van-tabbar-item>
+    </van-tabbar>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  data() {
+    return {
+      active: 0
+    };
+  }
+};
+</script>
