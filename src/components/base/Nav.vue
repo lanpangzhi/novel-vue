46a6cc6eb7c34c58b5b684459adebda05ba7c208
lanpangzhi <875727617@qq.com>
2018-06-03 21:17:07 +0800

    选中效果加强逻辑

--- src/components/base/Nav.vue
+++ src/components/base/Nav.vue
@@ -1,8 +1,6 @@
 <template>
-    <van-tabbar v-model="active">
-      <van-tabbar-item icon="home" to="/">首页</van-tabbar-item>
-      <van-tabbar-item icon="search" to="/serach">搜索</van-tabbar-item>
-      <van-tabbar-item icon="records" to="/serach">排行</van-tabbar-item>
+    <van-tabbar v-model="active" @change="change">
+      <van-tabbar-item v-for="item in data" :icon="item.icon" :to="item.path" :key="item.name">{{ item.name }}</van-tabbar-item>
     </van-tabbar>
 </template>
 
@@ -11,8 +9,48 @@ export default {
   name: "Nav",
   data() {
     return {
-      active: 0
+      active: 0,
+      data: [
+        {
+          name: "首页",
+          path: "/",
+          icon: "home",
+          n: 0
+        },
+        {
+          name: "搜索",
+          path: "/search",
+          icon: "search",
+          n: 1
+        },
+        {
+          name: "排行",
+          path: "/ranking",
+          icon: "records",
+          n: 2
+        }
+      ]
     };
+  },
+  created() {
+    let arr = this.data.filter(el => {
+      return el.path === this.$router.history.current.path;
+    });
+    this.change(arr[0].n);
+  },
+  watch: {
+    $route(from) {
+      let arr = this.data.filter(el => {
+        return el.path === from.path;
+      });
+
+      this.change(arr[0].n);
+    }
+  },
+  methods: {
+    change(n) {
+      this.active = n;
+    }
   }
 };
 </script>
