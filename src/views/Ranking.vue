4cbde8b0d31a18857b4c962bc6301924adbe8575
lanpangzhi <875727617@qq.com>
2018-06-03 21:20:08 +0800

    排行榜页面

---
+++ src/views/Ranking.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ranking">
+    <!-- 头部 -->
+    <Header title="排行榜" :isLeft="true" :isRight="true"/>
+    <!-- 头部 end -->
+    <!-- 导航 -->
+    <Nav/>
+    <!-- 导航 end -->
+  </div>
+</template>
+
+<script>
+import Header from "@/components/base/Header.vue";
+import Nav from "@/components/base/Nav.vue";
+
+export default {
+  name: "ranking",
+  components: {
+    Header,
+    Nav
+  }
+};
+</script>
