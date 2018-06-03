021c59bb53997904b3c175da3186737197c55526
lanpangzhi <875727617@qq.com>
2018-06-03 10:20:04 +0800

    增加动画过度效果

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="slide-right">
+      <router-view/>
+    </transition>
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      transitionName: ""
+    };
+  },
+  watch: {
+    $route(to, from) {
+      //如果to的索引值为0，不添加任何动画；如果to索引大于from索引,判断为前进状态,反之则为后退状态
+      if (to.meta.index > 0) {
+        if (to.meta.index < from.meta.index) {
+          this.transitionName = "slide-right";
+        } else {
+          this.transitionName = "slide-left";
+        }
+      } else if (to.meta.index == 0 && from.meta.index > 0) {
+        this.transitionName = "slide-right";
+      }
+    }
+  }
+};
+</script>
+
 
 <style lang="scss">
 #app {
@@ -11,10 +39,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
 }
 #nav {
   padding: 30px;
@@ -25,5 +49,28 @@
       color: #42b983;
     }
   }
+}
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active {
+  will-change: transform;
+  transition: all 0.5s;
+  transform: translateZ(0);
+  position: absolute;
+  width: 100%;
+  left: 0;
+}
+.slide-right-enter {
+  transform: translateX(-100%);
+}
+.slide-right-leave-active {
+  transform: translateX(100%);
+}
+.slide-left-enter {
+  transform: translateX(100%);
+}
+.slide-left-leave-active {
+  transform: translateX(-100%);
 }
 </style>
