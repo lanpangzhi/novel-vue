ab31db1deb855b0cbbf33017a8d56ebaf9f443ef
lanpangzhi <875727617@qq.com>
2018-06-03 21:18:24 +0800

    头部props存入data中

--- src/components/base/Header.vue
+++ src/components/base/Header.vue
@@ -1,19 +1,26 @@
 <template>
-    <van-nav-bar :title="title"  :left-arrow="left" fixed @click-left="onClickLeft" @click-right="onClickRight">
-        <van-icon name="search" slot="right" />
+    <van-nav-bar :title="tit" :z-index="99999"  :left-arrow="left" fixed @click-left="onClickLeft" @click-right="onClickRight">
+        <van-icon name="search" slot="right" v-if="right"/>
     </van-nav-bar>
 </template>
 
 <script>
 export default {
   name: "Header",
-  props: ["left", "title"],
+  props: ["isLeft", "title", "isRight"],
+  data() {
+    return {
+      left: this.isLeft,
+      tit: this.title,
+      right: this.isRight
+    };
+  },
   methods: {
     onClickLeft() {
       this.$router.go(-1);
     },
     onClickRight() {
-      this.$router.push("/serach");
+      this.$router.push("/search");
     }
   }
 };
