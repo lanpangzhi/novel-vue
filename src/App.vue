351147bbdcc3472a01c62b774b716631c3abc0f8
lanpangzhi <875727617@qq.com>
2018-06-04 16:18:43 +0800

    修改导航z-index值

--- src/App.vue
+++ src/App.vue
@@ -43,15 +43,8 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.van-tabbar--fixed {
+  z-index: 2000;
 }
 .slide-right-enter-active,
 .slide-right-leave-active,
