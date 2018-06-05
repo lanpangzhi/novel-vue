3fce545158f65436640cd9408823d0d6a7f088b3
lanpangzhi <875727617@qq.com>
2018-06-05 15:15:53 +0800

    修改loading样式

--- src/components/base/Loading.vue
+++ src/components/base/Loading.vue
@@ -1,27 +1,17 @@
 <template>
-  <van-loading color="white" v-if="off"/>
+  <van-loading/>
 </template>
 
 <script>
 export default {
-  name: "Loading",
-  props: {
-    off: Boolean
-  }
+  name: "Loading"
 };
 </script>
 
 <style scoped lang="scss">
-.van-loading--white {
-  position: fixed;
-  z-index: 88888;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  padding: 15px;
+.van-loading--black {
+  padding: 10px;
   border-radius: 5px;
   margin: auto;
-  background: rgba(0, 0, 0, 0.5);
 }
 </style>
