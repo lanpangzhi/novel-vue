ddc1226d8f8b10d5816c073e3973943f08afb1b8
lanpangzhi <875727617@qq.com>
2018-06-06 16:12:04 +0800

    书籍信息页面

---
+++ src/components/DetailList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="detail-list">
+    <div class="detail-list-pic">
+      <img :src="book.cover" />
+    </div>
+    <div class="detail-list-info">
+      <h2 class="detail-list-tit">{{ book.title }}</h2>
+      <p class="detail-list-sp1"><span>{{ book.author }}</span>|<span>{{ book.minorCate }}</span>|<span>{{ (book.wordCount/10000).toFixed(2) }}万字</span></p>
+      <p class="detail-list-time">更新时间: <span>{{ time }}</span></p>
+      <p class="detail-list-last">最新文章: {{ book.lastChapter }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailList",
+  props: ["book"],
+  computed: {
+    time() {
+      let dateStr = this.book.updated.split(".")[0];
+      return dateStr.replace("T", "  ");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.detail-list {
+  .detail-list-pic {
+    width: 140px;
+    height: 200px;
+    margin: 0 auto;
+    padding: 10px 0;
+    img {
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+      padding: 2px;
+      border: 1px solid #ccc;
+    }
+  }
+  .detail-list-info {
+    .detail-list-tit {
+      line-height: 28px;
+    }
+    .detail-list-sp1 {
+      line-height: 24px;
+      font-size: 14px;
+      span {
+        padding: 0 5px;
+        &:first-of-type {
+          color: #f60;
+        }
+      }
+    }
+    .detail-list-time {
+      line-height: 24px;
+      font-size: 14px;
+      span {
+        color: red;
+      }
+    }
+    .detail-list-last {
+      line-height: 24px;
+      font-size: 14px;
+    }
+  }
+}
+</style>
---
+++ src/views/Detail.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="detail">
+    <!-- 头部 -->
+    <Header :title="title" :isLeft="true" :isRight="true"/>
+    <!-- 头部 end -->
+    <div class="detail-con">
+      <!-- 加载loading -->
+      <Loading v-if="isLoading"/>
+      <!-- 加载loading end -->
+      <template v-if="!isLoading">
+        <!-- 详情列表 -->
+        <DetailList :book="book"/>
+        <!-- 详情列表 end -->
+        <v-touch @tap="toArticle">
+          <van-button type="danger" block size="small">开始阅读</van-button>
+        </v-touch>
+        <v-touch tag="a" class="more" @tap="toChapter">查看目录</v-touch>
+      </template>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import Header from "@/components/base/Header.vue";
+import Loading from "@/components/base/Loading.vue";
+import DetailList from "@/components/DetailList.vue";
+import { Toast } from "vant";
+import { mapMutations, mapState } from "vuex";
+
+export default {
+  name: "Detail",
+  components: {
+    Header,
+    Loading,
+    DetailList
+  },
+  data() {
+    return {
+      title: "",
+      isLoading: true,
+      book: {},
+      isOff: false,
+      source: {}
+    };
+  },
+  watch: {
+    $route(to) {
+      if (to.name === "detail") {
+        this.isLoading = true;
+        this.getDetail();
+      }
+    }
+  },
+  computed: mapState({
+    id: "id",
+    sourceId: "sourceId"
+  }),
+  created() {
+    this.getDetail();
+  },
+  methods: {
+    async getDetail() {
+      if (this.isOff) {
+        return;
+      }
+      this.isOff = true;
+      let id = this.$route.params.id;
+      let bookData = await this.axios.get("book/" + id);
+      if (bookData.flag === 0) {
+        Toast.clear();
+        Toast(bookData.msg);
+        return;
+      }
+      this.title = bookData.book.title;
+      this.book = bookData.book;
+      let sourceData = await this.axios.get("source/", {
+        params: {
+          id
+        }
+      });
+      if (sourceData.flag === 0) {
+        Toast.clear();
+        Toast(sourceData.msg);
+        return;
+      }
+      this.source = sourceData.books;
+      this.isOff = false;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 300);
+    },
+    ...mapMutations({
+      upId: "updateId",
+      upSourceId: "updateSourceId",
+      upTit: "updateTit"
+    }),
+    toChapter() {
+      this.setState();
+      this.$router.push("/chapter");
+    },
+    toArticle() {
+      alert("commit vuex toArticle");
+      this.setState();
+    },
+    setState() {
+      this.upId(this.book._id);
+      this.upTit(this.book.title);
+      this.upSourceId(this.source._id);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.detail-con {
+  margin-top: 56px;
+  padding-left: 10px;
+  padding-right: 10px;
+  .more {
+    display: inline-block;
+    height: 30px;
+    padding: 0 40px;
+    font-size: 14px;
+    line-height: 30px;
+    margin-top: 15px;
+    color: red;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+  }
+  .van-button--small {
+    margin-top: 10px;
+    font-size: 16px;
+  }
+}
+</style>
