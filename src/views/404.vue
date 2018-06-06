6b5fef87404d07cbf8c39724dd661cd62e8c336e
lanpangzhi <875727617@qq.com>
2018-06-06 16:57:45 +0800

    404页面

---
+++ src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+  
+</template>
+
+<script>
+import { Dialog } from 'vant';
+
+export default {
+  name: "404",
+  created() {
+    this.alert();
+  },
+  methods: {
+    alert() {
+      Dialog.alert({
+        message: "(小伙子|大姑凉)你来到一个没有书籍的荒芜地带。"
+      }).then(() => {
+        this.$router.push("/");
+      });
+    }
+  }
+}
+</script>
