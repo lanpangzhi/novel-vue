8339f3934f2a3891eb262523f47b62c21a09eb74
lanpangzhi <875727617@qq.com>
2018-06-07 13:06:50 +0800

    文章页面

---
+++ src/components/ArticleCon.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="article-con" v-html="data">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArticleCon",
+  props: ["data"]
+};
+</script>
+
+<style lang="scss" scoped>
+.article-con {
+  padding: 0 10px;
+  text-align: left;
+  line-height: 34px;
+  font-size: 16px;
+  text-indent: 2em;
+}
+</style>
---
+++ src/views/Article.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="article">
+     <Header :title="tit" :isLeft="true"/>
+     <div class="article-wrap">
+       <Loading v-if="isLoading"/>
+       <ArticleCon :data="article" v-if="!isLoading"/>
+     </div>
+     <div v-if="!isLoading" class="article-btn">
+       <v-touch tag="a" @tap="prev" class="article-prev" :class="{ disabled: isPrev }">上一页</v-touch>
+       <v-touch tag="a" @tap="next" class="article-next" :class="{ disabled: isNext }">下一页</v-touch>
+     </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/base/Header.vue";
+import Loading from "@/components/base/Loading.vue";
+import ArticleCon from "@/components/ArticleCon.vue";
+import { mapMutations, mapState } from "vuex";
+import { Toast } from "vant";
+
+export default {
+  name: "Article",
+  components: {
+    Header,
+    ArticleCon,
+    Loading
+  },
+  data() {
+    return {
+      currentN: 0,
+      link: "",
+      chapterlist: [],
+      tit: "",
+      article: "",
+      isLoading: true,
+      off: false,
+      isPrev: false,
+      isNext: false
+    };
+  },
+  created() {
+    this.pageInit();
+  },
+  computed: {
+    ...mapState({
+      list: "list",
+      n: "n"
+    })
+  },
+  watch: {
+    n() {
+      this.isLoading = true;
+      this.pageInit();
+    },
+    list() {
+      this.chapterlist = this.list.chapters;
+    }
+  },
+  methods: {
+    ...mapMutations({
+      upN: "updateN"
+    }),
+    async getArticle() {
+      if (this.off) {
+        return;
+      }
+      this.off = true;
+      let link = this.chapterlist[this.currentN].link;
+      let title = this.chapterlist[this.currentN].title;
+      let data = await this.axios.get("/article", {
+        params: {
+          link
+        }
+      });
+      if (data.flag === 0) {
+        Toast.clear();
+        Toast(data.msg);
+        this.tit = "出错了...";
+        this.article = "";
+        this.isLoading = false;
+      } else {
+        this.tit = title;
+        this.article = this.toHtml(data.chapter.body);
+        this.isLoading = false;
+      }
+      setTimeout(() => {
+        this.off = false;
+      }, 300);
+    },
+    pageInit() {
+      if (this.chapterlist.length <= 0) {
+        this.chapterlist = this.list.chapters;
+      }
+      this.currentN = this.n;
+      if (this.n >= this.list.chapters.length - 1) {
+        this.isNext = true;
+      } else {
+        this.isNext = false;
+      }
+      if (this.n <= 0) {
+        this.isPrev = true;
+      } else {
+        this.isPrev = false;
+      }
+      this.getArticle();
+    },
+    toHtml(str) {
+      // eslint-disable-next-line
+      let re = new RegExp("([^\x00-\xff])+\n", "g");
+      return str.replace(re, $1 => {
+        return `<p>${$1}</p>`;
+      });
+    },
+    next() {
+      if (this.isNext) {
+        return;
+      }
+      let n = this.currentN;
+      n++;
+      this.upN(n);
+    },
+    prev() {
+      if (this.isPrev) {
+        return;
+      }
+      let n = this.currentN;
+      n--;
+      this.upN(n);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.article-wrap {
+  margin-top: 56px;
+}
+.article-btn {
+  height: 30px;
+  line-height: 30px;
+  width: 200px;
+  margin: 0 auto;
+  padding: 5px 10px 5px 10px;
+  a {
+    padding: 0 15px;
+  }
+  .article-prev {
+    float: left;
+  }
+  .article-next {
+    float: right;
+  }
+  .disabled {
+    color: #ccc;
+  }
+}
+</style>
