df703c1e0e5a1bf06aaa0b62a3536cf69f580289
lanpangzhi <875727617@qq.com>
2019-04-14 14:38:14 +0800

    feat: 删除骨架屏

--- src/skeleton.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Skeleton from './Skeleton.vue';
-
-export default new Vue({
-  components: {
-    Skeleton,
-  },
-  render: h => h(Skeleton),
-});
--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const SkeletonWebpackPlugin = require('vue-skeleton-webpack-plugin');
+// const path = require('path');
+// const SkeletonWebpackPlugin = require('vue-skeleton-webpack-plugin');
 
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new SkeletonWebpackPlugin({
-        webpackConfig: {
-          entry: {
-            app: path.join(__dirname, './src/skeleton.js'),
-          },
-        },
-        minimize: true,
-        quiet: true,
-        router: {
-          mode: 'history',
-          routes: [
-            {
-              path: '/',
-              skeletonId: 'skeleton1'
-            }
-          ]
-        }
-      }),
-    ],
-  },
-};
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new SkeletonWebpackPlugin({
+//         webpackConfig: {
+//           entry: {
+//             app: path.join(__dirname, './src/skeleton.js'),
+//           },
+//         },
+//         minimize: true,
+//         quiet: true,
+//         router: {
+//           mode: 'history',
+//           routes: [
+//             {
+//               path: '/',
+//               skeletonId: 'skeleton1'
+//             }
+//           ]
+//         }
+//       }),
+//     ],
+//   },
+// };
