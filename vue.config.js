0bacc76f4bf5401b38285ccb79bef64802984581
lanpangzhi <875727617@qq.com>
2019-04-14 16:44:33 +0800

    feat: 更换饿了么骨架屏幕方案

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,25 @@
-// const path = require('path');
-// const SkeletonWebpackPlugin = require('vue-skeleton-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { SkeletonPlugin } = require('page-skeleton-webpack-plugin')
+const path = require('path')
 
-// module.exports = {
-//   configureWebpack: {
-//     plugins: [
-//       new SkeletonWebpackPlugin({
-//         webpackConfig: {
-//           entry: {
-//             app: path.join(__dirname, './src/skeleton.js'),
-//           },
-//         },
-//         minimize: true,
-//         quiet: true,
-//         router: {
-//           mode: 'history',
-//           routes: [
-//             {
-//               path: '/',
-//               skeletonId: 'skeleton1'
-//             }
-//           ]
-//         }
-//       }),
-//     ],
-//   },
-// };
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new SkeletonPlugin({
+        pathname: path.resolve(__dirname, './shell'), // 用来存储 shell 文件的地址
+        staticDir: path.resolve(__dirname, './dist'), // 最好和 `output.path` 相同
+        routes: ['/'], // 将需要生成骨架屏的路由添加到数组中
+        excludes: ['.van-nav-bar', '.van-tabbar']
+      })
+    ],
+  },
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV !== 'development') {
+      config.plugin('html').tap(opts => {
+        opts[0].minify.removeComments = false
+        return opts
+      })
+    }
+    
+  },
+};
